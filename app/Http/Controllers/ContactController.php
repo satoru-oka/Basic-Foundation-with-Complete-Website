3332ce02758d993933b89b5509dd2ba8b1e849c1
satoru <40satoruoka10@gmail.com>
2022-04-04 22:19:02 +0900

    TM-415 How to Make Controller And Get Url Request

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index(){
+        // echo "This is our Contact Page";
+        return view('contact');
+    }
+}
